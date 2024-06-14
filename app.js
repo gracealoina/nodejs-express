07c1058b32621be634ac74e8cc8e069effd56487
gracealoina <aloinagrace.sitepu@gmail.com>
2024-06-14 20:36:13 +0700

    instal nodemon & basic routing dengan express

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
-console.log('hello world')
 //npm i express => nputk instal library express dari npmjs, wajib di terminal
 //nama node-modules wajib untuk simpan modul2
 //perhatikan package-lock j,son dan package.json
+
+const express = require ('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send(`<h2>Hello World!</h2><hr>`)
+  })
+
+  app.get('/profil', (req, res) => {
+    res.send('<h1>Profil saya </h1>')
+  })
+  
+  app.listen(port, () => {
+    console.log(`app sudah siap ${port}`)
+  })
+
+  //npm i nodemon agar tidak perlu lg ctrl c//mmati hidupkan  sebelum mainkan lagi tahap fungsi berikutnya
+
