46579e2d1c4d3b598cae04c5111602c7e718561d
gracealoina <aloinagrace.sitepu@gmail.com>
2024-06-14 19:59:24 +0700

    instal & mysql2

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 console.log('hello world')
+//npm i express => nputk instal library express dari npmjs, wajib di terminal
+//nama node-modules wajib untuk simpan modul2
+//perhatikan package-lock j,son dan package.json
