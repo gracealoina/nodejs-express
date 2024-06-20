85a959be1e22d858a305f8182cf4c0d372a073c6
gracealoina <aloinagrace.sitepu@gmail.com>
2024-06-20 20:02:50 +0700

    detail satu karyawan

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const express = require ('express')
 const app = express()
 const port = 3000
 
+
 app.set('view engine', 'ejs') //setting penggunaan template engine untuk express
 app.set('views', './view-ejs') //setting penggunaan folder untuk menyimpan file.ejs
 
@@ -24,9 +25,28 @@ app.get('/', (req, res) => {
         posisi:'aa',
         perusahaan: 'bb',
         gaji: '9',
-
+        pajak:(this.gaji>8)?'gaji anda kena pajak': 'aman, ga perlu bayar pajak',
+       
     })
   })
+
+  app.get('/karyawan',async (req,res)=> {
+      const m_karyawan= require('./model/m_karyawan')
+      let dataview ={
+        semua_karyawan: await m_karyawan.get_semua_karyawan(),
+      }
+      res.render('karyawan/all', dataview)
+    
+  })
+
+  app.get('/karyawan/detail',async (req,res)=> {
+    const m_karyawan= require('./model/m_karyawan')
+    let dataview ={
+      detail_karyawan: await m_karyawan.get_satu_karyawan(),
+    }
+    res.render('karyawan/detail', dataview)
+  
+})
 
   
   app.listen(port, () => {
